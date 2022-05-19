19b6147b6dd7c7bc214088b35c6b3d3f43c739e6
Maung Htike <maunghtike1999@gmail.com>
2022-05-19 04:37:48 -0700

    add experiences and projects route

--- controllers/homeroute.js
+++ controllers/homeroute.js
@@ -11,4 +11,24 @@ router.get("/", async (req, res) => {
     }
 });
 
+// experiences page
+router.get("/experiences", async (req, res) => {
+    try {
+        res.render("experiences");
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// projects page
+router.get("/projects", async (req, res) => {
+    try {
+        res.render("projects");
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
